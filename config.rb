20cbf42c1b40cbc68f795c89dc11bea62ec550c2
kenkla <k@zva.us>
2015-03-05 14:43:22 -0500

    added variables.sass and layout styles

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -18,3 +18,4 @@ javascripts_dir = "javascripts"
 # line_comments = false
 
 preferred_syntax = :sass
+require 'compass-normalize'
